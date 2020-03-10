dd676ec5bce1cfc7757eb9d9103cc8bda38ad65e
Berkay Bindebir <berkaybindebir@gmail.com>
2020-03-10 18:38:37 +0300

    wip: migrate hapi-v19

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 test:
-	@node node_modules/lab/bin/lab -a code
+	@node node_modules/.bin/lab -a @hapi/code
 test-cov:
-	@node node_modules/lab/bin/lab -a code -t 100
+	@node node_modules/.bin/lab --coverage-all -a @hapi/code
 test-cov-html:
-	@node node_modules/lab/bin/lab -a code -r html -o coverage.html
+	@node node_modules/.bin/lab -a @hapi/code -r html -o coverage.html
 
 .PHONY: test test-cov test-cov-html
--- test/index.js
+++ test/index.js
@@ -1,53 +1,53 @@
 // Load modules
-
-var Code = require('code');
-var Hapi = require('hapi');
-var Hoek = require('hoek');
-var Lab = require('lab');
-var Path = require("path");
+const Hapi = require('@hapi/hapi');
+const Code = require('@hapi/code');
+const Lab = require('@hapi/lab');
+const Hoek = require('@hapi/hoek');
+const Path = require("path");
+const Vision = require('@hapi/vision');
 
 // Declare internals
-
-var internals = {};
+const internals = {};
 
 // Test shortcuts
+const lab = exports.lab = Lab.script();
 
-var lab = exports.lab = Lab.script();
-var describe = lab.describe;
-var it = lab.it;
-var before = lab.before;
-var after = lab.after;
-var beforeEach = lab.beforeEach;
-var afterEach = lab.afterEach;
-var expect = Code.expect;
+const { describe, it, beforeEach } = lab;
+
+const { expect } = Code;
 
 describe("Hapi Basic i18n", function () {
-    var server;
+    let server;
 
-    beforeEach(function (done) {
-        server = new Hapi.Server();
-        server.connection();
-        done();
+    beforeEach(async function () {
+        try {
+            server = new Hapi.Server();
+            await server.start();
+
+            console.log(server.inject);
+        } catch (error) {
+            console.error(error);
+        }
     });
-
-    it("returns valid localized strings for EN", function (done) {
-        server.register({
-            register: require("../"),
-            options: {
-                locale_path: "./test/languages",
-                default_language: "EN",
-                available_languages: ["EN", "TR"]
-            }
-        }, function (err) {
-            expect(err).to.not.exist();
-
+    
+    it("returns valid localized strings for EN", async function () {
+        try {
+            await server.register({
+                plugin: require("../"),
+                options: {
+                    locale_path: "./test/languages",
+                    default_language: "EN",
+                    available_languages: ["EN", "TR"]
+                }
+            })
+            
             server.route({
                 method: 'GET',
                 path: '/',
                 config: {
-                    handler: function (request, reply) {
+                    handler: function (request, h) {
                         expect(request.i18n).to.exist();
-                        var localized = {
+                        const localized = {
                             hello: request.i18n("Hello"),
                             say_hello_to: request.i18n("Say Hello To", "Isaac"),
                             number: request.i18n("1"),
@@ -55,144 +55,158 @@ describe("Hapi Basic i18n", function () {
                             xxx: request.i18n("XXX"),
                             not_exist: request.i18n("Hohoho")
                         };
-                        return reply({localized: localized});
+                        return localized
                     }
                 }
             });
-
-            server.inject("/", function (res) {
+    
+            await server.inject("/", function (res) {
                 expect(res.result).to.exist();
                 expect(res.result.localized).to.exist();
-                var localized = res.result.localized;
+                
+                const localized = res.result.localized;
+                
                 expect(localized.hello).to.equal("Hello!");
                 expect(localized.say_hello_to).to.equal("Hello Isaac!");
                 expect(localized.number).to.equal("Number 1");
                 expect(localized.xxx).to.equal("EN XX");
                 expect(localized.number_not_exist).to.equal("198");
                 expect(localized.not_exist).to.equal("Hohoho");
-                done();
             });
-        });
+        } catch (err) {
+            expect(err).to.not.exist();
+        }
     });
 
-    it("returns valid localized strings from default language when avaiable languages mismatch with default one", function (done) {
-        server.register({
-            register: require("../"),
-            options: {
-                locale_path: "./test/languages",
-                default_language: "EN",
-                available_languages: ["TR"]
-            }
-        }, function (err) {
-            expect(err).to.not.exist();
+    it("returns valid localized strings from default language when avaiable languages mismatch with default one", async function () {
+        try {
+            await server.register({
+                plugin: require("../"),
+                options: {
+                    locale_path: "./test/languages",
+                    default_language: "EN",
+                    available_languages: ["TR"]
+                }
+            })
 
             server.route({
                 method: 'GET',
                 path: '/',
                 config: {
-                    handler: function (request, reply) {
+                    handler: function (request, h) {
                         expect(request.i18n).to.exist();
-                        var localized = {
-                            hello: request.i18n("Hello")
-                        };
-                        return reply({localized: localized});
+
+                        const localized = { hello: request.i18n("Hello") };
+                        return localized
                     }
                 }
             });
 
-            server.inject("/", function (res) {
+            await server.inject("/", function (res) {
                 expect(res.result).to.exist();
                 expect(res.result.localized).to.exist();
-                var localized = res.result.localized;
+
+                const localized = res.result.localized;
+
                 expect(localized.hello).to.equal("Hello!");
-                done();
             });
-        });
+        } catch (err) {
+            expect(err).to.not.exist();
+        }
     });
 
-    it("returns valid localized strings for TR", function (done) {
-        server.register({
-            register: require("../"),
-            options: {
-                locale_path: "./test/languages",
-                default_language: "TR",
-                available_languages: ["EN", "TR"]
-            }
-        }, function (err) {
-            expect(err).to.not.exist();
+    it("returns valid localized strings for TR", async function () {
+        try {
+            await server.register({
+                plugin: require("../"),
+                options: {
+                    locale_path: "./test/languages",
+                    default_language: "TR",
+                    available_languages: ["EN", "TR"]
+                }
+            })
 
             server.route({
                 method: 'GET',
                 path: '/',
                 config: {
-                    handler: function (request, reply) {
+                    handler: function (request, h) {
                         expect(request.i18n).to.exist();
-                        var localized = {
+                        
+                        const localized = {
                             hello: request.i18n("Hello"),
                             say_hello_to: request.i18n("Say Hello To", "Isaac"),
                             number: request.i18n("1"),
                             xxx: request.i18n("XXX"),
                             not_exist: request.i18n("Hohoho")
                         };
-                        return reply({localized: localized});
+
+                        return localized
                     }
                 }
             });
 
-            server.inject("/", function (res) {
+            await server.inject("/", function (res) {
                 expect(res.result).to.exist();
                 expect(res.result.localized).to.exist();
-                var localized = res.result.localized;
+                
+                const localized = res.result.localized;
+                
                 expect(localized.hello).to.equal("Merhaba!");
                 expect(localized.say_hello_to).to.equal("Selam Isaac!");
                 expect(localized.number).to.equal("1 Numara");
                 expect(localized.xxx).to.equal("XXX");
                 expect(localized.not_exist).to.equal("Hohoho");
-                done();
-            });
-        });
+            );
+        } catch (err) {
+            expect(err).to.not.exist();
+        }
     });
 
-    it("returns ..", function (done) {
-        server.views({
-            engines: {
-                html: require("handlebars") // means .ext is .html
-            },
-            path: Path.join(__dirname, 'views'),
-            isCached: false
-        });
+    it("returns ..", async function () {
+        try {
+            await server.register(Vision);
+            await server.views({
+                engines: {
+                    html: require("handlebars") // means .ext is .html
+                },
+                path: Path.join(__dirname, 'views'),
+                isCached: false
+            });
 
-        server.register({
-            register: require("../"),
-            options: {
-                locale_path: "./test/languages",
-                default_language: "EN",
-                available_languages: ["EN", "TR"]
-            }
-        }, function (err) {
-            expect(err).to.not.exist();
+            await server.register({
+                plugin: require("../"),
+                options: {
+                    locale_path: "./test/languages",
+                    default_language: "EN",
+                    available_languages: ["EN", "TR"]
+                }
+            })
 
             server.route({
                 method: 'GET',
                 path: '/',
                 config: {
-                    handler: function (request, reply) {
+                    handler: function (request, h) {
                         expect(request.i18n).to.exist();
                         var localized = {
                             hello: request.i18n("Hello")
                         };
-                        return reply.view("index");
+                        return h.view("index");
                     }
                 }
             });
 
-            server.inject("/", function (res) {
+            await server.inject("/", function (res) {
                 var tokens = res.result.split("\n");
 
                 expect(tokens[2]).to.equal("Hello!");
                 expect(tokens[3]).to.equal("Hello Isaac!");
-                done();
+
             });
-        });
+        } catch (err) {
+            console.error(err);
+            expect(err).to.not.exist();
+        }
     });
-});
+})
